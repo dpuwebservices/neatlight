2b7525512513383a1aea726a4d03161dbb3e7768
Christine McClure <camcclure@gmail.com>
2017-05-05 16:04:32 -0500

    remove narrative and footer from show.php

--- spca-grisham/neatline/exhibits/show.php
+++ spca-grisham/neatline/exhibits/show.php
@@ -22,20 +22,3 @@
   <?php echo nl_getExhibitMarkup(); ?>
 </div>
 
-<div id="neatline-narrative" class="narrative">
-
-  <!-- Back Link -->
-  <a href="<?php echo url('neatline'); ?>" class="back">
-    <span class="arrow">&larr;</span> View all Exhibits
-  </a>
-
-  <!-- Narrative -->
-  <h1 class="title"><?php echo nl_getExhibitField('title'); ?></h1>
-  <?php echo nl_getExhibitField('narrative'); ?>
-
-  <!-- Colophon -->
-  <?php echo common('colophon'); ?>
-
-</div>
-
-<?php echo foot(); ?>
